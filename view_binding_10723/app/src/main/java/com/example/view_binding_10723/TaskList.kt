f376c680cf32046e40c4096e6d869507f0b74129
ArchangelaIndah <200710723@students.uajy.ac.id>
2022-09-14 16:58:09 +0700

    first commit

---
+++ view_binding_10723/app/src/main/java/com/example/view_binding_10723/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10723
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Bangun tidur tapi mager mau ngapa2in"),
+        Task("Chapter 2", "Scrool Tiktok", "Mencari video hiburan buat bangun mood"),
+        Task("Chapter 3", "Makan", "Biar nanti ga sakit asam lambung wkkwkw"),
+        Task("Chapter 4", "Ngopi", "Enaknya emang ngopi si kalau pagi-pagi gitu"),
+        Task("Chapter 5", "Ke kampus","Tetap belajar ngoding, walaupun cita2 ui/ux designer:)")
+    )
+}
